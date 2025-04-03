db35d77da8c066bb26c28079fab4d978f68e056a
m1kpe0 <yarikasik@inbox.ru>
2025-04-03 04:35:00 +0300

    Update auto_breaker_likee.Lua

--- auto_breaker_likee.lua
+++ auto_breaker_likee.lua
@@ -9,8 +9,8 @@ local function tp(cf)
 end
 
 Window:Toggle("Break parts", false, function(e)
-    bool = e
-    while bool do
+    bool1 = e
+    while bool1 do
         tp(CFrame.new(-34, 103, 38))
         task.wait(0.1)
         tp(CFrame.new(-34, 103, 24))
@@ -21,8 +21,8 @@ Window:Toggle("Break parts", false, function(e)
 end)
 
 Window:Toggle("Create my parts", false, function(e)
-    bool = e
-    if bool then
+    bool2 = e
+    if bool2 then
         for i, part in pairs(workspace:GetDescendants()) do
             if part:IsA("Part") and part:FindFirstChild("TouchInterest") then
                 local part_copy = Instance.new("Part", workspace)
@@ -45,11 +45,11 @@ Window:Toggle("Create my parts", false, function(e)
 end)
 
 Window:Toggle("Loop unfreeze", false, function(e)
-    bool = e
-    while bool do
+    bool3 = e
+    while bool3 do
         if me.Character ~= nil then 
             for i, v in pairs(me.Character:GetDescendants()) do
-                if v:IsA("BasePart") and v.Anchored then v.Anchored = false end
+                if v:IsA("BasePart") and v.Anchored and bool3 then v.Anchored = false end
             end
         end
         task.wait(0.1)
