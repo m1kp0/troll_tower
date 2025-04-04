a8f3eca56d6d44d48f993a54a4c3d5d4b76f6cd9
m1kpe0 <yarikasik@inbox.ru>
2025-04-04 03:30:34 +0300

    Update auto_breaker_likee.lua

--- auto_breaker_likee.lua
+++ auto_breaker_likee.lua
@@ -1,16 +1,19 @@
+local inf = math.huge
 local me = game.Players.LocalPlayer
+local zoom = me.CameraMaxZoomDistance
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/turtle"))()
-local Window = library:Window("LadderBreaker | Troll Tower")
+local loop_unfreeze_en, break_en, parts_en
+zoom = inf
 
+-- functions
 local function tp(cf)
     if me.Character ~= nil then
         me.Character.HumanoidRootPart.CFrame = cf
     end
 end
 
-Window:Toggle("Break parts", false, function(e)
-    bool1 = e
-    while bool1 do
+local function break_parts()
+    while break_en do
         tp(CFrame.new(-34, 103, 38))
         task.wait(0.1)
         tp(CFrame.new(-34, 103, 24))
@@ -18,12 +21,11 @@ Window:Toggle("Break parts", false, function(e)
         tp(CFrame.new(-35, 103, 10))
         task.wait(0.1)
     end
-end)
+end
 
-Window:Toggle("Create my parts", false, function(e)
-    bool2 = e
-    if bool2 then
-        for i, part in pairs(workspace:GetDescendants()) do
+local function manage_parts()
+    if parts_en then
+        for i, part in pairs(workspace:GetChildren()) do
             if part:IsA("Part") and part:FindFirstChild("TouchInterest") then
                 local part_copy = Instance.new("Part", workspace)
                 part_copy.BrickColor = part.BrickColor
@@ -31,27 +33,47 @@ Window:Toggle("Create my parts", false, function(e)
                 part_copy.CFrame = part.CFrame
                 part_copy.Size = part.Size
                 part_copy.Transparency = 0.5
-                part.Name = "LadderBreaker_defense_part"
+                part_copy.Name = "LadderBreaker_defense_part"
                 part_copy.Anchored = true
             end
         end
     else
-        for i, copy in pairs(workspace:GetDescendants()) do
-            if copy:IsA("Part") and copy.Name == "LadderBreaker_defense_part" then
+        for i, copy in pairs(workspace:GetChildren()) do
+            if copy.Name == "LadderBreaker_defense_part" then
                 copy:Destroy()
             end
         end
     end
-end)
+end
 
-Window:Toggle("Loop unfreeze", false, function(e)
-    bool3 = e
-    while bool3 do
+local function unfreeze_player()
+    while loop_unfreeze_en do
         if me.Character ~= nil then 
             for i, v in pairs(me.Character:GetDescendants()) do
-                if v:IsA("BasePart") and v.Anchored and bool3 then v.Anchored = false end
+                if v:IsA("BasePart") and v.Anchored then v.Anchored = false end
+                task.wait(0.1)
             end
         end
         task.wait(0.1)
     end
+end
+
+-- gui
+local Window = library:Window("LadderBreaker | TIAPT")
+
+Window:Toggle("Break parts", false, function(e)
+    break_en = e
+    break_parts()
 end)
+
+Window:Toggle("Create my parts", false, function(e)
+    parts_en = e
+    manage_parts()
+end)
+
+Window:Toggle("Loop unfreeze", false, function(e)
+    loop_unfreeze_en = e
+    unfreeze_player()
+end)
+
+Window:Label("Troll Is A Pinning Tower")
